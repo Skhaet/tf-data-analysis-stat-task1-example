275832211268a6e282a7b782a643061575729c51
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-06 23:40:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,10 @@
 import pandas as pd
 import numpy as np
 
+import statistics as s
 
 chat_id = 270620880 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    x.sort()
-    l = len(x)
-    
-    if l%2 == 0:
-        k = ((x[l//2]+x[l//2 + 1])/2)/(58**2)
-    else: k = (x[l//2])/(58**2)
-    #for i in range (l):
-    #    x[i] = (k)/(58**2)
-    
-    
-    return k # Ваш ответ
+
+    return s.median(x)/(58**2)
