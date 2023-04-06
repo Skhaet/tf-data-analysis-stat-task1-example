259eb3769a01058e12ec7d03f8b9bb8bfb24de9b
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-06 22:40:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,14 @@ import numpy as np
 chat_id = 270620880 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    for i in range (len(x)):
-        x[i] = (x[i])/(58**2)
-    return x.mean() # Ваш ответ
+    x.sort()
+    l = len(x)
+    
+    if l%2 == 0:
+        k = ((x[l//2]+x[l//2 + 1])/2)/(58**2)
+    else: k = (x[l//2])/(58**2)
+    #for i in range (l):
+    #    x[i] = (k)/(58**2)
+    
+    
+    return k # Ваш ответ
