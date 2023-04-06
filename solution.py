da6cef0f0872331b4183ec3d9c6e153e5fc34a1d
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-06 23:54:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import numpy as np
 
-import statistics as s
+import statistics as st
 
 chat_id = 270620880 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
 
-    return s.median(x)/(58**2)
+    line = st.median(x)
+    n = 0
+    s = 0
+    for i in range (len(x)):
+        if x[i]<=line:
+            s = s +x[i]
+            n = n+1
+    k = (s/n)/(58**2)
+    return k # Ваш ответ
