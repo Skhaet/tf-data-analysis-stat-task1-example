0387f25b6ae46720c770c18d6dc3174b57876851
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-06 21:42:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 270620880 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    for i in range (len(x)):
+        x[i] = (x[i])/(58**2)
     return x.mean() # Ваш ответ
